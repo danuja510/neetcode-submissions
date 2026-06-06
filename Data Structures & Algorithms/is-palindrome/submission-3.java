701d13fc167a2fce874b326921d5b06fdf7c73de
Danuja Kalugamaarachchi <59965846+danuja510@users.noreply.github.com>
2026-06-06 12:43:03 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toLowerCase().toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(c);
+            }
+        }
+        var s1 = cleaned.toString().toCharArray();
+        for(int i=0, j=s1.length-1; i< s1.length; i++, j--){
+            if(i==j || i>j) return true;
+            if(s1[i]!= s1[j]) return false;
+        }
+        return true;
+    }
+}
