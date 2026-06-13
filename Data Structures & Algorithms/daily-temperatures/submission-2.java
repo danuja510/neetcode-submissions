38015fb2032158d7f970fbd8f49b0d59b7b92ed6
Danuja Kalugamaarachchi <59965846+danuja510@users.noreply.github.com>
2026-06-13 16:27:17 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> pending = new Stack();
+        int[] output = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            if (i == temperatures.length - 1) {
+                output[i] = 0;
+            }
+            while (!pending.isEmpty() && temperatures[pending.peek()] < temperatures[i]) {
+                output[pending.peek()] = i - pending.peek();
+                pending.pop();
+            }
+            pending.push(i);
+        }
+        return output;
+    }
+}
