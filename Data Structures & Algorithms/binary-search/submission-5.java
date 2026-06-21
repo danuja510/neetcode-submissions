5941f4cc3205d3f8fbe000e57a9d7910330a4bcd
Danuja Kalugamaarachchi <59965846+danuja510@users.noreply.github.com>
2026-06-21 11:43:57 +0530

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        System.out.println(i + ","+ j);
+        while (i <= j) {
+            int selection = (j + i) / 2;
+            if (nums[selection] == target) {
+                return selection;
+            } else if (nums[selection] > target) {
+                j = selection - 1;
+            } else {
+                i = selection + 1;
+            }
+        }
+        return -1;
+    }
+}
