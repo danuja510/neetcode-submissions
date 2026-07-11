6dbedc9d616313eb76c0a2466757785b2b04a489
Danuja Kalugamaarachchi <59965846+danuja510@users.noreply.github.com>
2026-07-11 19:57:45 +0530

    Add: eating-bananas - submission-15

---
+++ Data Structures & Algorithms/eating-bananas/submission-15.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Arrays.stream(piles).max().getAsInt();
+        int returnK= max, r= max, tempH =0, l=1;
+        while (l <= r) {
+            int k = (l + r) / 2;
+            tempH = 0;
+            for (int j = 0; j < piles.length; j++) {
+                tempH += Math.ceilDiv(piles[j], k);
+            }
+            if (tempH <= h) {
+                if (returnK > k)
+                    returnK = k;
+            }
+            if (tempH <= h) {
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+        return returnK;
+    }
+}
